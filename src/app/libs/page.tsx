ae02735dcfabfe1f3ef8055e328cc588341dee9d
Rocketman35 <harhyom9189@gmail.com>
2025-01-17 15:17:29 +0900

    three.js page init

---
+++ src/app/libs/page.tsx
@@ -0,0 +1,10 @@
+import LinkLayout from "@/components/layouts/LinkLayout";
+import Link from "next/link";
+
+export default function LibsPracticePage() {
+  return (
+    <LinkLayout>
+      <Link href="/libs/three-js">Three.js library react version practice</Link>
+    </LinkLayout>
+  );
+}
